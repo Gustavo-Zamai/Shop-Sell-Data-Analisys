c87da39f37ca52632b5d4dda0075168f31ad3980
Gustavo Zamai <gustavosimaozamai@gmail.com>
2025-02-13 10:27:56 -0300

    Add project

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-# Lógica de Programação
-# Passo 0 - Entender o problema
-# Passo 1 - Percorrer a pasta Vendas para acessar os dados
 import os
+import pandas as pd
+import plotly.express as px
 
 data_list = os.listdir("./Vendas")
 #print(data_list)
 
-# Step 2 - Import sells database
-for data in data_list:
-    if "Vendas" in data:
-        print(f"./Vendas/{data}")
-    else :
-        print("Return file")
+joined_table = pd.DataFrame()
 
-# Step 3 - Treat the databases
+for file in data_list:
+    if "Vendas" in file:
+        table = pd.read_csv(f"./Vendas/{file}")
+        joined_table = joined_table._append(table)
 
-
-
+#print(joined_table)
 # Passo 4 - Calcular o produto mais vendido em quantidade
+product_table = joined_table.groupby("Produto").sum()
+
+## Select table to display
+product_table = product_table[["Quantidade Vendida"]].sort_values(by="Quantidade Vendida", ascending=False)
+print(product_table)
+
 # Passo 5 - Calcular o produto que mais faturou
+joined_table["Faturamento"] = joined_table["Quantidade Vendida"] * joined_table["Preco Unitario"]
+#print(joined_table)]
+invoicing_table = joined_table.groupby("Produto").sum()
+invoicing_table = invoicing_table[["Faturamento"]].sort_values(by="Faturamento", ascending=False)
+print(invoicing_table)
+
 # Passo 6 - Calcular a loja/cidade que mais vendeu (faturamento)
+store_table = joined_table.groupby("Loja").sum()
+store_table = store_table[["Faturamento"]].sort_values(by="Faturamento", ascending=False)
+print(store_table)
+
 # Passo 7 - Criar um dashboard
+graphic = px.bar(store_table,x=store_table.index ,y="Faturamento")
+graphic.show()
